eed59422fcbfe6e83f6baf4c081c1afd765c69dd
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2024-01-15 13:16:37 +0100

    made the form work

--- dist/process.php
+++ dist/process.php
@@ -1,15 +1,20 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == 'POST') { // Check if the User coming from a request
+if (isset($_POST["send"]) && $_SERVER['REQUEST_METHOD'] == 'POST') { // Check if the User coming from a request
+  echo "send is set" . "<br>";
   $name = $_POST["name"];
   $subject = $_POST["subject"];
-  $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL); // simple validation if you insert an email
-  $phone = $_POST["phone"];
-  $msg = $_POST["msg"]; // simple validation if you insert a string
+  // $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL); // simple validation if you insert an email
+  // $email = $_POST["email"]; // simple validation if you insert an email
+  // $phone = $_POST["phone"];
+  $msg = $_POST["msg"];
 
-  // mail function in php look like this  (mail(To, subject, Message, Headers, Parameters))
-  $headers = "FROM : " . $name . $email . $phone . "\r\n";
+
+  $msg = "From $name, $msg";
+
+  // $headers = "FROM : " . $name . $email . $phone . "\r\n";
   $myEmail = "dmitrii@malyshkin.net";
-  if (mail($myEmail, $subject, "message coming from the contact form", $msg, $headers)) {
+
+  if (mail($myEmail, $subject, $msg, $headers)) {
     echo "sent";
   } else {
     echo "error";
