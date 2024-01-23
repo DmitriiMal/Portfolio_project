130cd508c7be7741787183bcdd701af0769917cf
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2024-01-23 14:06:57 +0100

    Implement recaptcha into the contact form

--- dist/process.php
+++ dist/process.php
@@ -1,24 +1,41 @@
 <?php
-if (isset($_POST["send"]) && $_SERVER['REQUEST_METHOD'] == 'POST') { // Check if the User coming from a request
-  $name = $_POST["name"];
-  $subject = $_POST["subject"];
-  $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL); // simple validation if you insert an email
-  $phone = $_POST["phone"];
-  $msg = $_POST["msg"];
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+  $recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
+  $recaptcha_secret = '6LdNSVkpAAAAAL_TW7da0K7EiGlnXd5HX91NIBkv';
+  $recaptcha_response = $_POST['g-recaptcha-response'];
+
+  $recaptcha = file_get_contents($recaptcha_url . '?secret=' . $recaptcha_secret . '&response=' . $recaptcha_response);
+  $recaptcha = json_decode($recaptcha, true);
+
+  if ($recaptcha['success'] == 1 && $recaptcha['score'] >= 0.5 && $recaptcha['action'] == 'send') {
 
 
-  $msg = "From: $name\r\n $email\r\n $phone" . "\r\n" . "\r\n" . "$msg";
+    $name = $_POST["name"];
+    $subject = $_POST["subject"];
+    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL); // simple validation if you insert an email
+    $phone = $_POST["phone"];
+    $msg = $_POST["msg"];
 
-  // $headers = "FROM :  $name \r\n $email \r\n $phone \r\n";
-  // $headers = [$name, $email, $phone];
-  // $headers = "FROM : Test";
-  // $headers = "FROM : " . $name . $email . $phone . "\r\n";
-  $myEmail = "dmitrii@malyshkin.net";
+    $msg = "From: $name\r\n $email\r\n $phone" . "\r\n" . "\r\n" . "$msg";
 
-  if (mail($myEmail, $subject, $msg, $headers)) {
-    echo "sent";
-    header('Location: ' . 'contact.html');
+    //////// $headers = "FROM :  $name \r\n $email \r\n $phone \r\n";
+    //////// $headers = [$name, $email, $phone];
+    //////// $headers = "FROM : Test";
+    //////// $headers = "FROM : " . $name . $email . $phone . "\r\n";
+    $myEmail = "dmitrii@malyshkin.net";
+
+    if (mail($myEmail, $subject, $msg, $headers)) {
+      echo "sent";
+      header('Location: ' . 'contact.html');
+    } else {
+      echo "error";
+    }
   } else {
-    echo "error";
+    echo "something went wrong :(";
+    echo '<pre style="padding: 10px; background: #ccc; color: #333; border-radius: 4px">';
+    var_dump($recaptcha);
+    echo '</pre>';
   }
 }
